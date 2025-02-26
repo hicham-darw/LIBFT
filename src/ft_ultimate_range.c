4a1c44f3539c02f9d9e9444a1febb6d083bb39ac
hicham-darw <hamoudihicham@gmail.com>
2025-02-26 12:24:51 +0000

    fix func by norminette

--- src/ft_ultimate_range.c
+++ src/ft_ultimate_range.c
@@ -1,29 +1,40 @@
-#include	<stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elhahicham <hachemdarwin@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: YYYY/MM/DD HH:MM:SS by elhahicham        #+#    #+#             */
+/*   Updated: YYYY/MM/DD HH:MM:SS by elhahicham       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
 
 int	ft_ultimate_range(int **range, int min, int max)
 {
-	int *ptr;
-	int i;
+	int	*ptr;
+	int	i;
 
-	if(min >= max)
+	if (min >= max)
 	{
 		*(range) = NULL;
-		return 0;
+		return (0);
 	}
 	else
 	{
-		ptr = (int*)malloc( sizeof(int) * (max-min) );
-		if(!ptr)
-			return 0;
+		ptr = (int *)malloc(sizeof(int) * (max - min));
+		if (!ptr)
+			return (0);
 		i = 0;
-		while(min < max )
+		while (min < max)
 		{
 			ptr[i] = min;
 			min++;
 			i++;
 		}
-
-		*(range) = ptr;
-		return i;
+		(*range) = ptr;
+		return (i);
 	}
 }
