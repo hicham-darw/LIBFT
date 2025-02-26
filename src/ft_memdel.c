217659ce6b7435869d9f977698c735014aa41366
hicham-darw <hamoudihicham@gmail.com>
2025-02-26 22:55:47 +0000

    fix bugs

---
+++ src/ft_memdel.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elhahicham <hachemdarwin@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: YYYY/MM/DD HH:MM:SS by elhahicham        #+#    #+#             */
+/*   Updated: YYYY/MM/DD HH:MM:SS by elhahicham       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	ft_memdel(void **ap)
+{
+	if (!ap || !(*ap))
+		return ;
+	free(*ap);
+	(*ap) = NULL;
+}
