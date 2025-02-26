edea7dee8785ff93c82c60d9db96048ecee56d90
hicham-darw <hamoudihicham@gmail.com>
2025-02-26 12:20:41 +0000

    fix func by norminette

--- src/ft_ultimate_div_mod.c
+++ src/ft_ultimate_div_mod.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_div_mod.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elhahicham <hachemdarwin@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: YYYY/MM/DD HH:MM:SS by elhahicham        #+#    #+#             */
+/*   Updated: YYYY/MM/DD HH:MM:SS by elhahicham       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	int temp;
+	int	temp;
 
 	temp = *(a);
 	*(a) = temp / *(b);
